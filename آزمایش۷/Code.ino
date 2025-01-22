eeda809975399ca8c371aefb00bcc5f6844a607b
mobinapanah82 <panahmobina7@gmail.com>
2025-01-23 00:29:31 +0330

    Create Code.ino

---
+++ آزمایش۷/Code.ino
@@ -0,0 +1,15 @@
+int led = 13; 
+int but = 2;
+
+void setup() { 
+  pinMode(led, OUTPUT); 
+  pinMode(but, INPUT); 
+}
+
+void loop() {
+  int pinState = digitalRead(but); 
+  if(pinState == HIGH)
+{ 
+  digitalWrite(led, HIGH); } 
+  else 
+  { digitalWrite(led, LOW); } }
