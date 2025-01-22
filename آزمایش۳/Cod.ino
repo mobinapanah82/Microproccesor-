cc188cc83dad4f9295be96722958bdff705f5079
mobinapanah82 <panahmobina7@gmail.com>
2025-01-22 23:50:25 +0330

    Update Cod.ino

--- آزمایش۳/Cod.ino
+++ آزمایش۳/Cod.ino
@@ -1,1 +1,9 @@
 
+int led1 = 9; // تعریف پین LED اول 
+int led2 = 13; // تعریف پین LED دوم
+
+void setup() { pinMode(led1, OUTPUT); // تنظیم پین LED اول به عنوان خروجی 
+pinMode(led2, OUTPUT); // تنظیم پین LED دوم به عنوان خروجی }
+
+void loop() { digitalWrite(led1, LOW); // خاموش کردن LED اول 
+digitalWrite(led2, HIGH); // روشن کردن LED دوم }
