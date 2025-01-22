5654affe503db173152bb235c23ca56fc8053561
mobinapanah82 <panahmobina7@gmail.com>
2025-01-22 23:15:36 +0330

    Create Cod.ino

---
+++ آزمایش۲/Cod.ino
@@ -0,0 +1,19 @@
+led2 = 13; // تعریف پین LED دوم
+
+void setup() {
+
+pinMode(led1, OUTPUT); // تنظیم پین LED اول به عنوان خروجی
+
+pinMode(led2, OUTPUT); // تنظیم پین LED دوم به عنوان خروجی }
+
+void loop() {
+
+digitalWrite(led1, HIGH); // روشن کردن LED اول
+
+digitalWrite(led2, HIGH); // روشن کردن LED دوم
+
+delay(5000); // مکث به مدت 5 ثانیه digitalWrite(led1, LOW); // خاموش کردن LED اول
+
+digitalWrite(led2, LOW); // خاموش کردن LED دوم
+
+delay(500); // مکث به مدت 0.5 ثانیه }
